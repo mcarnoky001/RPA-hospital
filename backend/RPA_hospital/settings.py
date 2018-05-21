9f51c9c65a609a2c5f1a4e170525e7fa93c79ad7
git <asdf>
2018-05-21 14:43:32 +0200

    [TASK] set of postgresql database (you will need to create it on your pc)

--- backend/RPA_hospital/settings.py
+++ backend/RPA_hospital/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'RPA_hospital.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rpa-hospital',
+        'USER': 'django',
+        'PASSWORD': 'Python',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
